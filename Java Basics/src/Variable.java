252c81404f5a45d90f06b8363a7b4aac5b7b651a
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-03 19:52:08 +0530

    Second Commit

--- Java Basics/src/Variable.java
+++ Java Basics/src/Variable.java
@@ -1,7 +1,7 @@
 public class Variable {
     static void main(String[] args) {
         int a = 10;
-        int b= 20;
+        int b= 30;
         int c = a+b;
         System.out.println("Total sum of the Numbers is: "+c);
     }
