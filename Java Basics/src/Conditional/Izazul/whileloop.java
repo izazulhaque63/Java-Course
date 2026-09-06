abfd16d14eb1c3c905e05d088b2593c60b24bdd7
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-06 23:26:41 +0530

    06/09/2026

--- Java Basics/src/Conditional/Izazul/whileloop.java
+++ Java Basics/src/Conditional/Izazul/whileloop.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class whileloop {
    static void main(String[] args) {
-//        System.out.println("welcome to while loop");
-//        int num = 0;
-//        while (num <= 5){
-//            System.out.println(num);
-//            num++;
-//        }
+        System.out.println("welcome to while loop");
+        int num = 0;
+        while (num <= 5){
+            System.out.println(num);
+            num++;
+        }
 
-//        int num = 1;
-//        while(num<=10) {
-//            System.out.println(num);
-//            num++;
-//        }
+        int num = 1;
+        while(num<=10) {
+            System.out.println(num);
+            num++;
+        }
 
-//       int num = 10;
-//       while(num>=1) {
-//            System.out.println(num);
-//            num--;
-//        }
+       int num = 10;
+       while(num>=1) {
+            System.out.println(num);
+            num--;
+        }
 
 
 
-//       int num = 0;
-//       while(num<=20) {
-//           System.out.println(num);
-//           num = num +2;
-//       }
+       int num = 0;
+       while(num<=20) {
+           System.out.println(num);
+           num = num +2;
+       }
 
        System.out.println("multiplication table");
        Scanner input = new Scanner(System.in);
