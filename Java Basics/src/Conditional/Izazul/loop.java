c1154b3419b3b2455c3615d3259b1c59891b9b1f
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-06 23:23:24 +0530

    06/09/2026

--- Java Basics/src/Conditional/Izazul/loop.java
+++
@@ -1,9 +0,0 @@
-package Conditional.Izazul;
-
-public class loop {
-    static void main(String[] args) {
-        for (int i=0;i<=10;i++){
-            System.out.print(i+" ");
-        }
-    }
-}
---
+++ Java Basics/src/Conditional/Izazul/whileloop.java
@@ -0,0 +1,49 @@
+package Conditional.Izazul;
+
+import java.util.Scanner;
+
+public class whileloop {
+   static void main(String[] args) {
+//        System.out.println("welcome to while loop");
+//        int num = 0;
+//        while (num <= 5){
+//            System.out.println(num);
+//            num++;
+//        }
+
+//        int num = 1;
+//        while(num<=10) {
+//            System.out.println(num);
+//            num++;
+//        }
+
+//       int num = 10;
+//       while(num>=1) {
+//            System.out.println(num);
+//            num--;
+//        }
+
+
+
+//       int num = 0;
+//       while(num<=20) {
+//           System.out.println(num);
+//           num = num +2;
+//       }
+
+       System.out.println("multiplication table");
+       Scanner input = new Scanner(System.in);
+       System.out.println("please enter the number");
+
+
+
+       int number = 5;
+       while (number<=10){
+           System.out.println(number + "x" + number + "=" + (number * number));
+           number++;
+       }
+
+
+
+   }
+}
