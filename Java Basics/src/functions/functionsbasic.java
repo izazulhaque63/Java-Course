4742ebfb8018150a69f55f776ae32534b7e4e994
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-08 16:38:48 +0530

    08/09/2026

---
+++ Java Basics/src/functions/functionsbasic.java
@@ -0,0 +1,39 @@
+package functions;
+
+public class functionsbasic {
+    public static void main(String[] args) {
+        //sayHello();
+        //printname();
+        //printNumbers();
+        printEvenNumbers();
+
+    }
+//    public static void sayHello(){
+//        System.out.println("hii, how are you");
+   // }
+
+    //public static void printname(){
+      //  System.out.println("hello masoom");
+   // }
+
+//   public static void printNumbers() {
+//        int numbers = 1;
+//        while(numbers <= 10){
+//            System.out.println(numbers);
+//            numbers++;
+//        }
+
+   // }
+
+    public static void printEvenNumbers() {
+        int i = 1;
+        while (i / 2 == 0) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+
+
+
