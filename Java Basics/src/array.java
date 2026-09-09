d9124efd0f1a96463664b4b4158eefa6ceefc008
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-09 15:08:35 +0530

    09/09/2026

---
+++ Java Basics/src/array.java
@@ -0,0 +1,36 @@
+public class array {
+    public static void main(String[] args) {
+        System.out.println("welcome to array");
+//        int[] myarray = new int[5];
+//        myarray[0] = 98;
+//        myarray[1] = 94;
+//        myarray[2] = 93;
+//        myarray[3] = 95;
+//        myarray[4] = 96;
+
+//        System.out.println(myarray[0]);
+//        System.out.println(myarray[1]);
+//        System.out.println(myarray[2]);
+//        System.out.println(myarray[3]);
+//        System.out.println(myarray[4]);
+
+
+
+//        int[] myarray = {98, 94, 93, 95,96};
+//
+//        System.out.println(myarray[0]);
+//        System.out.println(myarray[1]);
+//        System.out.println(myarray[2]);
+//        System.out.println(myarray[3]);
+//        System.out.println(myarray[4]);
+
+
+        int[] myarray = {98, 94, 93, 95,96};
+        int index = 2;
+        System.out.println(myarray[0]);
+        System.out.println(myarray[1]);
+        System.out.println(myarray[index]);
+        System.out.println(myarray[3]);
+        System.out.println(myarray[4]);
+    }
+}
