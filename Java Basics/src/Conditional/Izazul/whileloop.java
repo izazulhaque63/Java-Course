78f977b80cd7c8b2787281f5abf43c182d643669
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-12 15:29:45 +0530

    12/09/2026

--- Java Basics/src/Conditional/Izazul/whileloop.java
+++ Java Basics/src/Conditional/Izazul/whileloop.java
@@ -62,20 +62,30 @@ public class whileloop {
       // }
 
 
+//       Scanner input = new Scanner(System.in);
+//       System.out.println("add of thr number");
+//       System.out.println("please enter the number ");
+//       int number = input.nextInt();
+//       int i = 1;
+//       int sum = 0;
+//       while(i <= 10){
+//           sum = sum + i;
+//           i++;
+//       }
+//       System.out.println("sum = " + sum);
+
+
        Scanner input = new Scanner(System.in);
-       System.out.println("add of thr number");
-       System.out.println("please enter the number ");
-       int number = input.nextInt();
+       System.out.println("please enter your number");
+       int n = input.nextInt();
        int i = 1;
        int sum = 0;
-       while(i <= 10){
+       while (i <= 5){
            sum = sum + i;
            i++;
+           //System.out.println("your total sum is "+ sum);
        }
-       System.out.println("sum = " + sum);
-
-
-
-
+       System.out.println("your total sum is "+ sum);
    }
+
 }
--- Java Basics/src/array.java
+++ Java Basics/src/array.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
-        System.out.println("welcome to array");
+        //     System.out.println("welcome to array");
 //        int[] myarray = new int[5];
 //        myarray[0] = 98;
 //        myarray[1] = 94;
@@ -15,7 +15,6 @@ public class array {
 //        System.out.println(myarray[2]);
 //        System.out.println(myarray[3]);
 //        System.out.println(myarray[4]);
-
 
 
 //        int[] myarray = {98, 94, 93, 95,96};
@@ -52,7 +51,6 @@ public class array {
 //        System.out.println(array[3]);
 
 
-
 //        int[] myArray = new int[3];
 //        myArray[0] = 9;
 //        myArray[1] = 3;
@@ -66,10 +64,7 @@ public class array {
 //        while(index < array.length){
 //            System.out.println(index);
 //            index++;
- //       }
-
-
-
+        //       }
 
 
 //                Scanner input = new Scanner(System.in);
@@ -90,40 +85,46 @@ public class array {
 //                input.close();
 
 
-        Scanner input = new Scanner(System.in);
-        int[] arr = new int[4];
-        int i = 0;
-        System.out.println("print 4 elements array");
-        while(i < arr.length){
-            System.out.println("enter element at index " + i + ":");
-            arr[i] = input.nextInt();
-            i++;
-        }
-        i = 0;
-        System.out.println("array element are ");
-        while (i < arr.length){
-            System.out.println(arr[i]);
-            i++;
-        }
-
-
-
-
-
-
 //        Scanner input = new Scanner(System.in);
-//        System.out.println("please enter your number");
-//        int n = input.nextInt();
-//        int i = 1;
-//        int sum = 0;
-//        while (i <= n){
-//            sum = sum + i;
+//        int[] arr = new int[4];
+//        int i = 0;
+//        System.out.println("print 4 elements array");
+//        while(i < arr.length){
+//            System.out.println("enter element at index " + i + ":");
+//            arr[i] = input.nextInt();
 //            i++;
-//            //System.out.println("your total sum is "+ sum);
 //        }
-//        System.out.println("your total sum is "+ sum);
-//            }
+//        i = 0;
+//        System.out.println("array element are ");
+//        while (i < arr.length){
+//            System.out.println(arr[i]);
+//            i++;
 //        }
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter your number");
+        int n = input.nextInt();
+        int i = 1;
+        int sum = 0;
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+            //System.out.println("your total sum is "+ sum);
+        }
+        System.out.println("your total sum is " + sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
