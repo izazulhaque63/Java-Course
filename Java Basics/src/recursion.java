095e2ec0b8261a58a37464c7c5a706804829e3e6
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-21 13:43:10 +0530

    21/09/2026

--- Java Basics/src/recursion.java
+++ Java Basics/src/recursion.java
@@ -1,3 +1,16 @@
-public void main(String[] args) {
-    System.out.println("welcome to recursive example");
+public class recursion{
+    public static void showNumber(int num){
+        if (num == 0){
+            return ;
+        }
+        System.out.println("num");
+        showNumber(num - 1);
+    }
+    public void main(String[] args) {
+        showNumber(5);
+    }
 }
+
+
+
+
