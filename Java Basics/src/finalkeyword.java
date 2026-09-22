438f837036558c7ade25893462828deba9e0e9f5
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-22 16:23:44 +0530

    22/09/2026

---
+++ Java Basics/src/finalkeyword.java
@@ -0,0 +1,10 @@
+public class finalkeyword {
+    public static void main(String[] args) {
+//        final int age = 20;
+//
+//        age = 25;
+//        System.out.println(age);
+
+
+    }
+}
---
+++ Java Basics/src/strBuilder.java
@@ -0,0 +1,56 @@
+public class strBuilder {
+    public static void main(String[] args) {
+//        StringBuilder sb = new StringBuilder("first");
+//        sb.append(45);
+//        sb.append("add karte jao");
+//        sb.append(76.77);
+//        sb.append(3333);
+//        //sb.toString();
+//        System.out.println(sb);
+
+
+        //insert()
+
+        //Specific index par value insert karta hai.
+
+//        StringBuilder builder = new StringBuilder("Jva");
+//
+//        builder.insert(1, "a");
+//
+//        System.out.println(builder);
+
+
+
+        //replace()
+
+        //Specific range replace karta hai.
+//        StringBuilder builder = new StringBuilder("I like Java");
+//
+//        builder.replace(7, 11, "Python");
+//
+//        System.out.println(builder);
+
+       // delete()
+
+        //Characters ki range delete karta hai.
+
+//        StringBuilder builder = new StringBuilder("Java Programming");
+//
+//        builder.delete(4, 16);
+//
+//        System.out.println(builder);
+
+
+
+
+        //deleteCharAt()
+
+        //Ek character delete karta hai.
+
+//        StringBuilder builder = new StringBuilder("JJava");
+//
+//        builder.deleteCharAt(0);
+//
+//        System.out.println(builder);
+    }
+}
