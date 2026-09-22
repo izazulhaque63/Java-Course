9eaca77e656a1de356d9104aa286874f5d71251c
izazulhaque63 <izazulgithubproject@gmail.com>
2026-09-22 16:03:00 +0530

    22/09/2026

---
+++ Java Basics/src/strClas.java
@@ -0,0 +1,36 @@
+public class strClas {
+    public static void main(String[] args) {
+//        String name = "rahul ";
+//        name = name.concat("kumar");
+//        System.out.println(name);
+
+
+
+//        String text = "java";
+//        System.out.println(text.length());
+
+//        String text = "java";
+//        System.out.println(text.charAt(0));
+//        System.out.println(text.charAt(3));
+
+
+//        String text = "programing";
+//        System.out.println(text.substring(4));
+//        System.out.println(text.substring(0,5));
+
+
+//        String a = "Java";
+//        String b = "Java";
+//
+//        System.out.println(a.equals(b));
+
+
+//        String text = "Java,Python,C++";
+//
+//        String[] languages = text.split(",");
+//
+//        for (String language : languages) {
+//            System.out.println(language);
+        }
+    }
+
